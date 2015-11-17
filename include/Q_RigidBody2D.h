2126f13d7aa242a3442b60f7423eba9a8cbe30af
Ahnaf Siddiqui <luky1971@users.noreply.github.com>
2015-11-16 20:40:41 -0500

    Created rigidbody2d and quantumworld2d

---
+++ include/Q_RigidBody2D.h
@@ -0,0 +1,34 @@
+/*
+	Copyright 2015 Ahnaf Siddiqui
+ 
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+ 
+	http://www.apache.org/licenses/LICENSE-2.0
+ 
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+#ifndef Q_RIGID_BODY_2D_H
+#define Q_RIGID_BODY_2D_H
+
+#include "D_Vector2i.h"
+
+namespace Quantum2D {
+	class RigidBody2D {
+		public:
+		Diamond::Vector2i velocity;
+		
+		RigidBody2D(int transform_index);
+		
+		private:
+		int transform_index;
+	};
+}
+
+#endif /* Q_RIGID_BODY_2D_H */
