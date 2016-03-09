cb65c3ba00acdcf86699afa4a1df6c1222b2be93
Ahnaf Siddiqui <luky1971@users.noreply.github.com>
2016-03-09 15:25:31 -0500

    Created collision functions matrix

--- src/Collision/Q_CollisionTest2D.cpp
+++ src/Collision/Q_CollisionTest2D.cpp
@@ -1,32 +1,55 @@
 /*
-	Copyright 2015 Ahnaf Siddiqui
+    Copyright 2015 Ahnaf Siddiqui
  
-	Licensed under the Apache License, Version 2.0 (the "License");
-	you may not use this file except in compliance with the License.
-	You may obtain a copy of the License at
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
  
-	http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
  
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 */
 
 #include "Q_CollisionTest2D.h"
 
 #include <typeindex>
 
+typedef bool(*ColFunc)(Quantum2D::Collider2D*, Quantum2D::Collider2D*);
+
+namespace Quantum2D {
+    namespace CollisionTest2D {
+        static ColFunc col_funcs[eNUMCOLTYPES][eNUMCOLTYPES];
+        
+        static bool colNONE(Collider2D *a, Collider2D *b) {
+            return false;
+        }
+        
+        static bool colAABB2(Collider2D *a, Collider2D *b) {
+            return AABB2(static_cast<AABBCollider2D*>(a), static_cast<AABBCollider2D*>(b));
+        }
+    }
+}
+
 void Quantum2D::CollisionTest2D::init() {
     // TODO: initialize a 2D array of function pointers to collision test functions addressed by collider types
+    col_funcs[eNONE][eNONE] = colNONE;
+    for (int i = eNONE + 1; i < eNUMCOLTYPES; ++i) {
+        col_funcs[eNONE][i] = colNONE;
+        col_funcs[i][eNONE] = colNONE;
+    }
+    
+    col_funcs[eAABB][eAABB] = colAABB2;
 }
 
 bool Quantum2D::CollisionTest2D::collide(Collider2D *a, Collider2D *b) {
-    return false;
+    return col_funcs[a->getType()][b->getType()](a, b);
 }
 
 bool Quantum2D::CollisionTest2D::AABB2(Quantum2D::AABBCollider2D *a, Quantum2D::AABBCollider2D *b) {
-	return !(a->getMin().x - b->getMax().x > 0.0f || a->getMin().y - b->getMax().y > 0.0f
-			|| b->getMin().x - a->getMax().x > 0.0f || b->getMin().y - a->getMax().y > 0.0f);
+    return !(a->getMin().x - b->getMax().x > 0.0f || a->getMin().y - b->getMax().y > 0.0f
+            || b->getMin().x - a->getMax().x > 0.0f || b->getMin().y - a->getMax().y > 0.0f);
 }
--- src/Q_Rigidbody2D.cpp
+++ src/Q_Rigidbody2D.cpp
@@ -1,17 +1,17 @@
 /*
-	Copyright 2015 Ahnaf Siddiqui
+    Copyright 2015 Ahnaf Siddiqui
  
-	Licensed under the Apache License, Version 2.0 (the "License");
-	you may not use this file except in compliance with the License.
-	You may obtain a copy of the License at
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
  
-	http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
  
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 */
 
 #include "Q_Rigidbody2D.h"
@@ -19,7 +19,7 @@
 #include "Q_QuantumWorld2D.h"
 
 Quantum2D::Rigidbody2D::Rigidbody2D(transform2_id transform)
-	: velocity(), transform(transform) {}
+    : velocity(), transform(transform) {}
 
 
 void Quantum2D::Rigidbody2D::update(tD_delta delta_ms) {
